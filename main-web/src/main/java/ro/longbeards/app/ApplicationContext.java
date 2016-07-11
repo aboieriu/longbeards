188644112a1b06d9c7ac44df4a6b12e6eb6293d5
aboieriu <aboieriu@pentalog.fr>
2016-07-11 15:56:24 +0300

    Base commit

---
+++ main-web/src/main/java/ro/longbeards/app/ApplicationContext.java
@@ -0,0 +1,7 @@
+package ro.longbeards.app;
+
+/**
+ * Created by aboieriu on 7/11/16.
+ */
+public class ApplicationContext {
+}
---
+++ main-web/src/main/java/ro/longbeards/service/DwarvesService.java
@@ -0,0 +1,7 @@
+package ro.longbeards.service;
+
+/**
+ * Created by aboieriu on 7/11/16.
+ */
+public class DwarvesService {
+}

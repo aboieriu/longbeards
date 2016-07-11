6290a50fd427f5e4a1e7624b6eefe050e042166e
aboieriu <aboieriu@pentalog.fr>
2016-07-11 15:56:49 +0300

    Setup of longbeards app

--- main-web/src/main/java/ro/longbeards/app/ApplicationContext.java
+++ main-web/src/main/java/ro/longbeards/app/ApplicationContext.java
@@ -1,7 +1,18 @@
 package ro.longbeards.app;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
 /**
  * Created by aboieriu on 7/11/16.
  */
+@Configuration
+@ComponentScan(basePackages = {"ro.longbeards"})
+@EnableAutoConfiguration
 public class ApplicationContext {
+    public static void main(String[] args) {
+        org.springframework.context.ApplicationContext ctx = SpringApplication.run(ApplicationContext.class, args);
+    }
 }
--- main-web/src/main/java/ro/longbeards/service/DwarvesService.java
+++ main-web/src/main/java/ro/longbeards/service/DwarvesService.java
@@ -1,7 +1,17 @@
 package ro.longbeards.service;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 /**
  * Created by aboieriu on 7/11/16.
  */
+@RestController
+@RequestMapping("/dwarves")
 public class DwarvesService {
+
+    @RequestMapping()
+    public String dwarvesIndex() {
+        return "Greetings : show dwarves";
+    }
 }
